c988dcb0c9be00153f859df4a9c687ccee41a274
andreFatec4 <37981911+andreFatec4@users.noreply.github.com>
2019-08-23 16:50:47 -0300

    Create aula2ExProp3.py

---
+++ aula2ExProp3.py
@@ -0,0 +1,9 @@
+print ("-----------Ano Bissexto------------")
+
+ano = int(input("Informe o ano a pesquisar: "))
+if(ano%4 == 0):
+    print ("Sim! Esse ano foi/é/será Bissexto")
+else:
+    print ("Não! Esse ano não foi/é/será Bissexto")
+
+print ("--------Obrigado por Participar---------")
